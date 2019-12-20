fb4d26388afdd8a023001d908688cbaa02ec5fc1
Damien <mail@damientimewell.com>
2019-12-20 14:20:56 +0100

    Adding express starter

---
+++ README.md
@@ -0,0 +1,4 @@
+# Express Starter Project
+
+This project includes the packages and babel setup for an express server, and is just meant to make things a little simpler to get up and running with.
+
---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+// Defines the port the app will run on. Defaults to 8080, but can be 
+// overridden when starting the server. For example:
+//
+//   PORT=9000 npm start
+const port = process.env.PORT || 8080
+const app = express()
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors())
+app.use(bodyParser.json())
+
+// Start defining your routes here
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`)
+})
